6b8fc43a86147be36ba03c00b339d7cc704d1a24
Christophe Kafrouni <ckafrouni@proton.me>
2023-12-28 11:50:45 +0100

    Implement fundamentals.FList

--- src/main/java/fundamentals/FList.java
+++ src/main/java/fundamentals/FList.java
@@ -8,13 +8,13 @@ import java.util.function.Predicate;
 
 /**
  * Author: Pierre Schaus
- *
+ * <p>
  * Functional Programming is an increasingly important programming paradigm.
  * In this programming paradigm, data structures are immutable.
  * We are interested here in the implementation of an immutable list
  * called FList allowing to be used in a functional framework.
  * Look at the main method for a small example using this functional list
- *
+ * <p>
  * Complete the implementation to pass the tests
  *
  * @param <A>
@@ -28,15 +28,15 @@ public abstract class FList<A> implements Iterable<A> {
             list = list.cons(i);
         }
 
-        list = list.map(i -> i+1);
+        list = list.map(i -> i + 1);
         // will print 10,9,...,1
-        for (Integer i: list) {
+        for (Integer i : list) {
             System.out.println(i);
         }
 
-        list = list.filter(i -> i%2 == 0);
+        list = list.filter(i -> i % 2 == 0);
         // will print 10,...,6,4,2
-        for (Integer i: list) {
+        for (Integer i : list) {
             System.out.println(i);
         }
     }
@@ -54,7 +54,13 @@ public abstract class FList<A> implements Iterable<A> {
     // return the length of the list
     public final int length() {
         // TODO
-         return -1;
+        return lengthAcc(0);
+    }
+
+    private int lengthAcc(int acc) {
+        if (this instanceof Nil) return acc;
+
+        return this.tail().lengthAcc(acc + 1);
     }
 
     // return the head element of the list
@@ -74,33 +80,41 @@ public abstract class FList<A> implements Iterable<A> {
     }
 
     // return a list on which each element has been applied function f
-    public final <B> FList<B> map(Function<A,B> f) {
+    public final <B> FList<B> map(Function<A, B> f) {
         // TODO
-         return null;
+        if (isEmpty()) return nil();
+
+        return new Cons<>(f.apply(head()), tail().map(f));
     }
 
     // return a list on which only the elements that satisfies predicate are kept
     public final FList<A> filter(Predicate<A> f) {
         // TODO
-         return null;
+        if (isEmpty()) return nil();
+
+        if (f.test(head())) return new Cons<>(head(), tail().filter(f));
+
+        return tail().filter(f);
     }
 
 
     // return an iterator on the element of the list
     public Iterator<A> iterator() {
         return new Iterator<A>() {
-            // complete this class
-
             FList<A> current = FList.this;
 
             public boolean hasNext() {
                 // TODO
-                 return false;
+                return current.tail().isNotEmpty();
             }
 
             public A next() {
                 // TODO
-                 return null;
+                if (!hasNext()) throw new NoSuchElementException();
+
+                A a = current.head();
+                current = current.tail();
+                return a;
             }
 
             public void remove() {
@@ -116,34 +130,38 @@ public abstract class FList<A> implements Iterable<A> {
         @Override
         public A head() {
             // TODO
-             return null;
+            throw new IllegalArgumentException();
         }
 
         @Override
         public FList<A> tail() {
             // TODO
-             return null;
+            throw new IllegalArgumentException();
         }
     }
 
     private static final class Cons<A> extends FList<A> {
 
         // TODO add instance variables
+        private A a;
+        private FList<A> tail;
 
 
         Cons(A a, FList<A> tail) {
+            this.a = a;
+            this.tail = tail;
         }
 
         @Override
         public A head() {
             // TODO
-             return null;
+            return this.a;
         }
 
         @Override
         public FList<A> tail() {
             // TODO
-             return null;
+            return this.tail;
         }
     }
 
