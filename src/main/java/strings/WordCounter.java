2f3eebdfbb03daeca6306e1cc27a3e3476600127
Christophe Kafrouni <ckafrouni@proton.me>
2023-12-28 12:17:06 +0100

    Implement strings.WordCounter

--- src/main/java/strings/WordCounter.java
+++ src/main/java/strings/WordCounter.java
@@ -11,26 +11,29 @@ import java.util.TreeMap;
  */
 public class WordCounter implements Iterable<String> {
 
+    private Map<String, Integer> wordCount;
 
     public WordCounter() {
+        wordCount = new TreeMap<>();
     }
 
     /**
      * Add the word so that the counter of the word is increased by 1
      */
     public void addWord(String word) {
+        wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
     }
 
     /**
      * Return the number of times the word has been added so far
      */
     public int getCount(String word) {
-         return -2;
+         return wordCount.getOrDefault(word, 0);
     }
 
     // iterate over the words in ascending lexicographical order
     @Override
     public Iterator<String> iterator() {
-         return null;
+         return wordCount.keySet().iterator();
     }
 }
