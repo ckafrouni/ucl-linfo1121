c225c09c0b69f2a9e102f858d52a05cf4534a3fd
Christophe Kafrouni <ckafrouni@proton.me>
2023-12-28 18:12:48 +0100

    Implement sorting.BinaryHeap

--- TODO.md
+++ TODO.md
@@ -1,12 +1,20 @@
 # TodoList
 
+---
+
 * [ ] Priority Queues
   * [ ] Implementation
   * [ ] Complexity
 
+A priority queue is a special type of queue data structure where each element is associated with a
+priority. The priority of the elements in a priority queue determines the deletion order. The element with the
+highest priority is deleted first. In case elements have the same priority, they are served according to their
+ordering in the queue.
+
+---
 
 * [ ] Incremental Hash Algorithm (Rolling hash ?)
-  * [ ] Rabin-Karp implementation
+    * [ ] Rabin-Karp implementation
 
 
 * [ ] etc...
--- src/main/java/sorting/BinaryHeap.java
+++ src/main/java/sorting/BinaryHeap.java
@@ -3,20 +3,20 @@ package sorting;
 /**
  * In this task, you must implement the `push` operation on a binary heap data structure.
  * As a reminder, a heap is a tree data structure such that the following invariant is respected
- *  
+ *
  *      For any node in the tree, the value associated with the node is higher (for a maxHeap) or lower
  *      (for a minHeap) than the value of its children.
  *
  * As a consequence, the minimum/maximum value is located at the root and can be retrieved in constant time.
  * In particular, this invariant must be respected after a push (or remove) operation.
  *
- * In this exercise the tree is represented with an array. The parent-child relation is implicitely represented
+ * In this exercise the tree is represented with an array. The parent-child relation is implicitly represented
  * by the indices. A node at index i has its two children at index 2i and 2i+1.
  * For this is it assumed that the root is located at index 1 in the array.
  */
 public class BinaryHeap {
 
-    private int [] content;
+    private int[] content;
     private int size;
 
     public BinaryHeap(int initialSize) {
@@ -25,10 +25,10 @@ public class BinaryHeap {
     }
 
     /**
-     * Doubles the available size of this binary heap
+     * Doubles the available size of this binary heap (min heap)
      */
     private void increaseSize() {
-        int [] newContent = new int[this.content.length*2];
+        int[] newContent = new int[this.content.length * 2];
         System.arraycopy(this.content, 0, newContent, 0, this.content.length);
         this.content = newContent;
     }
@@ -40,6 +40,24 @@ public class BinaryHeap {
      * @param value the added value
      */
     public void push(int value) {
+        if (this.size == this.content.length - 1) increaseSize();
+
+        this.size++;
+
+        // add the new value at the end
+        this.content[this.size] = value;
+
+        // fix the tree's invariant
+        int currentIndex = this.size;
+        int parrentIndex = this.size / 2;
+
+        while (currentIndex > 1 && this.content[currentIndex] < this.content[parrentIndex]) {
+            int temp = this.content[currentIndex];
+            this.content[currentIndex] = this.content[parrentIndex];
+            this.content[parrentIndex] = temp;
+            currentIndex = parrentIndex;
+            parrentIndex = currentIndex / 2;
+        }
     }
 
     /**
