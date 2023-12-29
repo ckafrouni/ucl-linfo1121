33af36b7f1ee783f25002ccd9e2eea9f5f34087f
Christophe Kafrouni <ckafrouni@proton.me>
2023-12-29 23:37:12 +0100

    Implements sorting.ClosestPair NOT EFFICIENT ENOUGH

--- src/main/java/sorting/ClosestPair.java
+++ src/main/java/sorting/ClosestPair.java
@@ -1,5 +1,7 @@
 package sorting;
 
+import java.util.Arrays;
+
 /**
  * Let a be an array of integers.
  * In this exercise we are interested in finding
@@ -17,12 +19,37 @@ package sorting;
 public class ClosestPair {
 
     /**
-      * Finds the pair of integers which sums up to the closest value of x
-      *
-      * @param a the array in which the value are looked for
-      * @param x the target value for the sum
-      */
-    public static int[] closestPair(int [] a, int x) {
-         return null; // TODO
+     * Finds the pair of integers which sums up to the closest value of x
+     *
+     * @param a the array in which the value are looked for
+     * @param x the target value for the sum
+     */
+    public static int[] closestPair(int[] a, int x) {
+        System.out.println(Arrays.toString(a));
+        Arrays.sort(a);
+
+        int nOp = 0;
+
+        int[] closestPair = new int[]{a[0], a[0]};
+        int closestDiff = Math.abs(a[0] + a[0] - x);
+
+        for (int j : a) {
+            for (int k : a) {
+                nOp++;
+
+                int diff = Math.abs(j + k - x);
+                if (diff < closestDiff) {
+                    closestPair = new int[]{j, k};
+                    closestDiff = diff;
+                }
+                if (closestDiff == 0) break;
+            }
+            if (closestDiff == 0) break;
+        }
+
+        System.out.println("size = " + a.length);
+        System.out.println("nOp = " + nOp);
+        System.out.println("-");
+        return closestPair;
     }
 }
